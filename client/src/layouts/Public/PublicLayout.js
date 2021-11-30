f92d826e578d8064112a3d741fbc976b28ecaede
faizan2002-npm <faizanblink34@gmail.com>
2021-11-30 14:44:03 +0500

    Header and Footer Customized

--- client/src/layouts/Public/PublicLayout.js
+++ client/src/layouts/Public/PublicLayout.js
@@ -1,4 +1,4 @@
-
+import { useEffect, useState } from 'react';
 import Header from '../../components/Public/Headers/Header';
 import Footer from '../../components/Public/Footers/Footer';
 import "../../assets/scss/Public/style.scss";
@@ -6,6 +6,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'react-phone-input-2/lib/style.css'
 
 const PublicLayout = ({ children }) => {
+    const adjuster = () => {
+        document.getElementById("main_content").style.paddingTop = `${document.getElementById("header").offsetHeight}px`;
+        document.addEventListener("DOMContentLoaded", function (event) {
+            document.getElementById("main_content").style.paddingTop = `${document.getElementById("header").offsetHeight}px`;
+        });
+        window.addEventListener('resize', function (event) {
+            document.getElementById("main_content").style.paddingTop = `${document.getElementById("header").offsetHeight}px`;
+        }, true);
+    }
+    useEffect(() => {
+        adjuster();
+    }, [])
     return (
         <>
             <Header />
