8ff7690f4cf2209fd4276b6eff7937468d48eccc
Faizan Shamas <faizanblink34@gmail.com>
2022-01-26 16:27:36 +0500

    Latest Code
    In this commit I have increased the client max body upload size.

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.set("view engine", "html");
 //// Body Parser Middleware ////
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit:'50mb'})); 
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
 
 //// Cookie Parser ////
 app.use(cookieParser());
